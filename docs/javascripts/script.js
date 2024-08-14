f39b1c20a2bbfdccc7b3e6e8400cbb15549ac568
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-08-14 11:56:00 +0000

    updated intersection to look for all class names containing 'intersect'

--- docs/javascripts/script.js
+++ docs/javascripts/script.js
@@ -1,14 +1,20 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
+        let entryClasses = Array.from(entry.target.classList.values())
+        let intersects = entryClasses.filter(class_ => class_.includes("intersect") );
         if (entry.isIntersecting) {
-            entry.target.classList.add('fade-in-up--active');
+            intersects.forEach((intersect_class) => {
+                entry.target.classList.add(intersect_class + '--active');
+            })
         } else {
-            entry.target.classList.remove('fade-in-up--active');
+            intersects.forEach((intersect_class) => {
+                entry.target.classList.remove(intersect_class + '--active');
+            })
         }
     });
 });
   
-const fade_in_ups = document.querySelectorAll('.fade-in-up');
+const fade_in_ups = document.querySelectorAll('.intersect-fade-in-up');
 fade_in_ups.forEach( fade_in_up => {
     observer.observe(fade_in_up);
 });
