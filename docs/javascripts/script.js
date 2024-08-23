551e4573af8bc83b2059356fd71fe989cbebb902
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-08-23 16:01:18 +0000

    added classes and setup function

--- docs/javascripts/script.js
+++ docs/javascripts/script.js
@@ -22,3 +22,44 @@ const intersects = document.querySelectorAll('[class*="intersect"]');
 intersects.forEach( intersect => {
     observer.observe(intersect);
 });
+
+
+let currentScrollPosition = window.scrollY;
+let navBarTabs = document.body.querySelector("nav.md-tabs");
+let header = document.body.querySelector("header.md-header");
+let headerTitle = document.body.querySelector("header .md-header__title");
+
+setUpNavBar = () => {
+  navBarTabs.classList.add("md-tabs--show");
+  navBarTabs.hidden = false;
+  header.classList.remove("md-header--shadow");
+  headerTitle.classList.remove("md-header__title--active");
+}
+document.addEventListener('DOMContentLoaded', setUpNavBar, false);
+
+
+window.addEventListener('scroll', () => {
+  
+  const newScrollPosition = window.scrollY;
+
+  if (newScrollPosition < currentScrollPosition) {
+    console.log("scrolling up")
+
+    header.classList.remove("md-header--shadow");
+    headerTitle.classList.remove("md-header__title--active");
+
+    navBarTabs.hidden = false;
+    navBarTabs.classList.add("md-tabs--show");
+  } else {
+    console.log("scrolling down")
+
+    header.classList.add("md-header--shadow");
+
+    headerTitle.classList.add("md-header__title--active")
+    
+    navBarTabs.hidden = true;
+    navBarTabs.classList.remove("md-tabs--show");
+  }
+
+  currentScrollPosition = newScrollPosition;
+});
